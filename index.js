fab5005b178c4f7fbd5bc1c453d00a45d7195646
Felipe <felipe.sarmiento.as@gmail.com>
2023-08-20 12:12:11 -0400

    complited all the main menu inquires

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const mainMenu = () => {
                 type: "list",
                 message: "What would you like to do?",
                 name: "selection",
-                choices: ["View all departments", "View all roles", "view all employees", new inquirer.Separator(), "Add a department", "Add a role", "Add an employee", new inquirer.Separator(), "Udpate an employee role", new inquirer.Separator()]
+                choices: ["View all departments", "View all roles", "view all employees", new inquirer.Separator(), "Add a department", "Add a role", "Add an employee", new inquirer.Separator(), "Udpate an employee role", new inquirer.Separator(), "Exit", new inquirer.Separator()]
             }
         ]).then((res) => {
             console.log(res);
@@ -78,8 +78,112 @@ function displayData(userChoice) {
                 askToContinue();
             });
             break;
+        case "Add a department":
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What is the name of the new Department?",
+                        name: "newDepartment"
+                    }]).then((res) => {
+                        console.log(res);
+                        db.query(`INSERT INTO department(name)
+                        VALUE ("${res.newDepartment}");`);
+                        db.query('SELECT id, name AS departments FROM department;', function (err, results) {
+                            console.table(results);
+                            askToContinue();
+                        });
+                    });
+            break;
+        case "Add a role":
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What is the name of the new Role?",
+                        name: "newRole"
+                    },
+                    {
+                        type: "number",
+                        message: "What is the salary?",
+                        name: "salary"
+                    },
+                    {
+                        type: "number",
+                        message: "What is the department ID?",
+                        name: "dep_ID"
+                    }
+                ]).then((res) => {
+                    console.log(res);
+                    db.query(`INSERT INTO role(title, salary, department_id)
+                        VALUE ("${res.newRole}", "${res.salary}", "${res.dep_ID}");`);
+                    db.query('SELECT id, title AS role, salary, department_id FROM role;', function (err, results) {
+                        console.table(results);
+                        askToContinue();
+                    });
+                });
+            break;
+        case "Add an employee":
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What is the first name of the new employee?",
+                        name: "firstName"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the last name of the new employee?",
+                        name: "lastName"
+                    },
+                    {
+                        type: "number",
+                        message: "What is the ID of its role?",
+                        name: "role"
+                    },
+                    {
+                        type: "number",
+                        message: "What is the ID of its manager?",
+                        name: "manager_ID"
+                    }
+                ]).then((res) => {
+                    console.log(res);
+                    db.query(`INSERT INTO employee(first_name, last_name,role_id,manager_id)
+                        VALUE ("${res.firstName}", "${res.lastName}", "${res.role}","${res.manager_ID}");`);
+                    db.query(`SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.name as department, ee.first_name as manager_name
+                        FROM employee as e
+                        JOIN role as r
+                        on r.id = e.role_id
+                        JOIN department as d
+                        on d.id = r.department_id
+                        LEFT JOIN employee as ee
+                        on e.manager_id = ee.id`, function (err, results) {
+                        console.table(results);
+                        askToContinue();
+                    });
+                });
+            break;
+        case "Udpate an employee role":
+            inquirer
+                .prompt([
+                    {
+                        type: "list",
+                        message: "Select Employee",
+                        name: "employee",
+                        choices: ["Velma", "shaggy"]
+                    },
+                    {
+                        type: "list",
+                        message: "What is the new role of the employee?",
+                        name: "newRole",
+                        choices: ["Cleaner", "Warehouse Manager"]
+                    },
+                ]).then((res) => {
+                    console.log(res);
+                });
+            break;
         default:
-            process.exit(0);
+         process.exit(0);
     }
 
 
