ce58b0923ece222ea8d427d2fee00fb7ac95d759
Felipe <felipe.sarmiento.as@gmail.com>
2023-08-20 11:10:22 -0400

    create a looping code for questions that goes back to the beginning or ends the application

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 // Import and require mysql2
 const mysql = require('mysql2');
+// import library to format tables
+const cTable = require('console.table');
 
 
 console.log(`
@@ -23,22 +25,56 @@ console.log(`
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'holafo$1515',
-      database: 'employees_db'
+        host: 'localhost',
+        user: 'root',
+        password: 'holafo$1515',
+        database: 'employees_db'
     },
     console.log(`Connected to the employee_db database.`)
-  );
+);
 
-inquirer
-.prompt([
-    {
-        type: "list",
-        message: "What would you like to do?",
-        name: "shapes",
-        choices: ["View all departments", "View all roles", "view all employees", new inquirer.Separator(), "Add a department", "Add a role","Add an employee", new inquirer.Separator(), "Udpate an employee role", new inquirer.Separator()]
-    }
-]).then((res) =>{
-    console.log(res);
-});
+const mainMenu = () => {
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "selection",
+                choices: ["View all departments", "View all roles", "view all employees", new inquirer.Separator(), "Add a department", "Add a role", "Add an employee", new inquirer.Separator(), "Udpate an employee role", new inquirer.Separator()]
+            }
+        ]).then((res) => {
+            console.log(res);
+            displayData(res.selection);
+
+        });
+}
+
+function displayData(userChoice) {
+
+    db.query('SELECT id, name AS departments FROM department;', function (err, results) {
+        console.table(results);
+        askToContinue();
+    });
+
+   
+
+}
+
+function askToContinue() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Back to Main Menu?",
+                name: "continue"
+            }
+        ]).then((res) => {
+            if (!res.continue) {
+                process.exit(0);
+            }
+            mainMenu();
+        });
+}
+
+mainMenu();
